a79fcd3310cf000899fc5aa0df926b35d381f21f
Smit Mehta <mehta.smit92@gmail.com>
2019-08-15 15:12:41 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return("Hello World!")
+    return render_template("home.html")
 
 @app.route("/me")
 def hello_me():
