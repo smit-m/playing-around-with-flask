6df5dba78d7fe04d03b7407f3de2e55aacd9e08a
Smit Mehta <mehta.smit92@gmail.com>
2019-08-15 14:48:15 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return("Hello World!")
+
+@app.route("/me")
+def hello():
+    return("Hello Smit!")
+    
+
+if __name__ == "__main__":
+    app.run()
